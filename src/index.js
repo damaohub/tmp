6af3124e6781adb8673f2a00e010e4ed2b4fa581
毛晨辉 <damao100@126.com>
2018-11-01 00:25:22 +0800

    add the basename for router in build

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={'/demo1'}>
             <App />
         </BrowserRouter>
     </Provider>
